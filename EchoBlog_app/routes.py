01d59b2ba0033ad31dc8ece6ff4559555d43f9c7
Toby16 <bolajibaasit14@gmail.com>
2023-10-11 00:05:32 -0400

    EchoBlog App | Report: Created endpoint or user login | Implemented basic validation of user data using FlaskForm

--- EchoBlog_app/routes.py
+++ EchoBlog_app/routes.py
@@ -1,5 +1,6 @@
 from EchoBlog_app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from EchoBlog_app.forms import LoginForm
 
 
 @app.route("/")
@@ -28,3 +29,20 @@ def index():
             user=user,
             posts=posts
     )
+
+
+@app.route("/login", methods=["GET", "POST"])
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    """
+    Route for user login
+    """
+    form = LoginForm()  # implement flask-forms on the frontend
+
+    if form.validate_on_submit():  # run all validations on input fields
+        print(form.data)
+        flash("Login requested for user: {}, remember_me: {}".format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+
+    return render_template("login.html", form=form, title="Sign In")
