d1d9d548746f8f305494abf962f646a1f4b6a1d7
Toby16 <bolajibaasit14@gmail.com>
2023-10-12 18:44:31 -0400

    EchoBlog App | Report: Included Post Model

--- EchoBlog_app/models.py
+++ EchoBlog_app/models.py
@@ -1,4 +1,5 @@
 from EchoBlog_app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -9,6 +10,26 @@ class User(db.Model):
     username = db.Column(db.String(60), unique=True, index=True)
     email = db.Column(db.String(120), unique=True, index=True)
     password = db.Column(db.String(200))
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self):
         return "user -> '{}'".format(self.username)
+
+
+class Post(db.Model):
+    """
+    Model for user's 'post' table in database
+    """
+    id = db.Column(db.Integer(), primary_key=True)
+    body = db.Column(db.String())
+    timestamp = db.Column(
+                    db.DateTime(), index=True,
+                    default=datetime.utcnow
+                )
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        if len(self.body) <= 20:
+            return "post -> '{}'".format(self.body)
+        return "post -> '{}'".format(self.body[:20] + "...")
+
