6dce94e7cd92ce0d799f41f85a0bf229a9150e9f
Toby16 <bolajibaasit14@gmail.com>
2023-10-12 11:35:46 -0400

    EchoBlog App | Report: Included configurations for sqlalchemy database

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
+# main directory of the application
+basedir_ = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or ("sqlite:///" + os.path.join(basedir_, "app.db"))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
