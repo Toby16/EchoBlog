2c4a1857d8e0e83cb7d40b921643cd57eaf85f31
Toby16 <bolajibaasit14@gmail.com>
2023-10-12 22:51:50 -0400

    EchoBlog App | Report: Included shell context for flask shell

--- echoblog.py
+++ echoblog.py
@@ -1,4 +1,14 @@
-from EchoBlog_app import app
+from EchoBlog_app import app, db
+from EchoBlog_app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """
+    a shell context that adds the database instance and models
+    to the flask shell session
+    """
+    return {"db": db, "User": User, "Post": Post}
 
 if __name__ == "__main__":
     app.run(debug=True)
