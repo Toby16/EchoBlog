21d3791a4806b0fa7e1f03e0bfcfe21b54ad8ff8
Toby16 <bolajibaasit14@gmail.com>
2023-10-15 05:20:15 -0400

    forms | Report: Implemented FlaskForm for user account registration | Included validation for username and signup

--- EchoBlog_app/forms.py
+++ EchoBlog_app/forms.py
@@ -1,9 +1,14 @@
+
 from flask_wtf import FlaskForm
 from wtforms import (
         StringField, SubmitField,
         PasswordField, BooleanField
     )
-from wtforms.validators import DataRequired
+from wtforms.validators import (
+        DataRequired, Email,
+        EqualTo, ValidationError
+    )
+from EchoBlog_app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -12,3 +17,28 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember me")
     submit = SubmitField("Log in")
+
+class RegistrationForm(FlaskForm):
+    # Get user details to create a new account
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    retype_password = PasswordField("Re-type Password", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Create account")
+
+    def validate_username(self, username):
+        """
+        method to check if username already exists
+        """
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Username not available!")
+
+
+    def validate_email(self, emai):
+        """
+        method to check if email already exists
+        """
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different email address!")
