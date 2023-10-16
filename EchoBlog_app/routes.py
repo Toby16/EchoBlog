fa3038a2daa437a0b4a80181afd40fa4412b8227
Toby16 <bolajibaasit14@gmail.com>
2023-10-15 22:10:22 -0400

    EchoBlog App | Report: Made a lot of changes i don't remember

--- EchoBlog_app/routes.py
+++ EchoBlog_app/routes.py
@@ -1,10 +1,11 @@
 from EchoBlog_app import app, db
 from flask import (render_template, flash, redirect, url_for, request)
-from EchoBlog_app.forms import LoginForm, RegistrationForm
+from EchoBlog_app.forms import (LoginForm, RegistrationForm, EditProfileForm)
 from EchoBlog_app.models import User
 from flask_login import (current_user, login_user,
                          logout_user, login_required)
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route("/")
@@ -85,8 +86,8 @@ def logout():
     return redirect(url_for("index"))
 
 
-@app.route("/register")
-@app.route("/register/")
+@app.route("/register", methods=["GET", "POST"])
+@app.route("/register/", methods=["GET", "POST"])
 def register():
     """
     view function for user account registeration
@@ -99,8 +100,58 @@ def register():
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
 
-        db.session.add(u)
+        db.session.add(user)
         db.session.commit()
         flash("Congratulations, you are now a registered user!")
         return redirect(url_for("login"))
     return render_template("register.html", form=form)
+
+
+@app.route("/user/<username>")
+@app.route("/user/<username>/")
+def user(username):
+    """
+    view function for user profile page
+    """
+    username = str(username)
+    user = User.query.filter_by(username=username).first_or_404()
+
+    posts = [
+        {"author": user, "body": "Hello World!, I code in Python"},
+        {"author": user, "body": "Using Flask Framework to build web applications"}
+    ]
+    return render_template("user.html", user=user, posts=posts, title="Profile")
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        """
+        including 'db.session.add()' is not necessary here because,
+        when you reference current_user,
+        Flask-Login will invoke the user loader callback function,
+        which will run a database query that will put the target user in the database session
+        """
+        db.session.commit()
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@app.route("/edit_profile/", methods=["GET", "POST"])
+def edit_profile():
+    """
+    view function to edit user profile
+    """
+    form = EditProfileForm()
+
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved successfully!")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template("edit_profile.html", title="Edit Profile", form=form)
